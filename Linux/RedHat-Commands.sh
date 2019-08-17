ed523cc81f42c248ceebee8cf83c37119ab0bcf0
rstoyanov <stoyanov.radostin@gmail.com>
2019-08-17 17:20:55 +0300

    Some more commands

--- Linux/RedHat-Commands.sh
+++ Linux/RedHat-Commands.sh
@@ -80,21 +80,98 @@ chmod o+r
 chmod a+r testfile
 
 # Change ownership of a file
-sudo chown username testfile
+chown username testfile
 
 # Change group ownership of a file
-sudo chgrp groupname testfile
+chgrp groupname testfile
+
+# Change username and group ownerwhip of a file with one command
+chown username:group testfile
 
 # Help commands
 whatis ssh
 man ssh
 ssh --help
 
-# Write output to a file (Overwrites the last file content!!!)
+# Write output to a file (Overwrites the latest file content!!!)
 echo "Write some text inside" > testfile
 
 # Append text to a file
 echo "Write some more text inside" >> testfile
 
+# Outputs the result to a file as well as it prints it to the screen (Overwrites the latest file content)
+echo "This is done by using tee command" | tee testfile2
+
+# Append text to a file using tee -a command
+echo "This is appended text done by using tee -a command" | tee -a testfile2
+
 # Output a result of a command to a file
 ls -ltr > resultfile
+
+# Returns count of bytes, characters, words, etc. -c -m -w
+wc -c testfile2
+
+# A command that returns the same output like ls -l
+ll
+
+# Get results one page at a time
+ls -ltr | more
+more testfile 
+
+# Displays content from a file
+more testfile # Gets content one page at a time
+head testfile # Gets top lines
+less testfile # Allow you to scroll one line at a time
+cat testfile # Prints the whole content
+tail testfile # Prints last lines 
+
+
+head -10 testfile # Displays 10 rows on the top
+tail -20 testfile # Displays 20 rows on the bottom
+tail -f testile # Displays in real time (--follow)
+
+# Shows cutted text from a file
+cut -c1 testfile # displays firt letters per line from a file
+cut -c1-2 testfile # returns the range of letters
+
+# Shows specific column from a file
+awk '{print $1}' testfile # Shows the first column
+awk '{print $2}' testfile # Shows the second column
+
+# Sorts the output
+awk '{print $1}' | sort
+
+# Displays only uniq content
+awk '{print $1}' | sort | uniq # It has to be sorted firts, otherwise uniq doesn't work
+
+# Compare two files 
+diff testfile testfile2 # Compares words
+cmp testfile testfile2 # Compares bytes
+
+# Archive files in tar
+tar -cvf testfile.tar /home/centos/abc* # archives all files starting with abc
+
+# Extract from tar archive
+tar -xvf testfile.tar
+
+# Compress tar files
+gzip testfile.tar
+
+# Decompress files 
+gzip -d testfile.tar.gz
+
+# View the content of a zipped text file
+zcat testfile.gz
+
+# Combine 2 files in 1
+cat file1 file2 > newfile
+
+# Split files 
+split -l 2 newfile sepfile # Exports each 2 lines to a separate file
+
+# Replace content of a file with another content
+sed 's/OldContnt/NewContent/g' tesftile
+
+
+
+
