481f218c6b1fde62b1c935d73693fb5ec0c9a7fa
rstoyanov <stoyanov.radostin@gmail.com>
2020-06-11 00:57:23 +0300

    Working Email funtion. Tested with cronjob

--- Python/chsi_scrapper/main.py
+++ Python/chsi_scrapper/main.py
@@ -1,13 +1,15 @@
-
 import requests
 from bs4 import BeautifulSoup
 
 URL = 'http://sales.bcpea.org/bg/properties.html?court=28&type=8&city=1'
+#URL = 'http://sales.bcpea.org/bg/properties.html?court=28&type=3&city=1'
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 results = soup.find(id='content')
-job_elems = results.find_all('ul', {'class':'results_list'})
+job_elems = results.find_all('li')
+
+result = ""
 
 for job_elem in job_elems:
     title = job_elem.h2.a.text
@@ -16,51 +18,9 @@ for job_elem in job_elems:
     details = " ".join(job_elem.p.text.split())
     link = job_elem.find('a').get('href')
     link = "http://sales.bcpea.org" + link
-    print(title)
-    print(price)
-    print(details)
-    print(link)
-
-### Insert into MySQL 
-"""
-import mysql.connector
-
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="yourusername",
-  password="yourpassword",
-  database="mydatabase"
-)
-
-mycursor = mydb.cursor()
-
-sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
-val = [
-  ('Peter', 'Lowstreet 4'),
-  ('Amy', 'Apple st 652'),
-  ('Hannah', 'Mountain 21'),
-  ('Michael', 'Valley 345'),
-  ('Sandy', 'Ocean blvd 2'),
-  ('Betty', 'Green Grass 1'),
-  ('Richard', 'Sky st 331'),
-  ('Susan', 'One way 98'),
-  ('Vicky', 'Yellow Garden 2'),
-  ('Ben', 'Park Lane 38'),
-  ('William', 'Central st 954'),
-  ('Chuck', 'Main Road 989'),
-  ('Viola', 'Sideway 1633')
-]
-
-mycursor.executemany(sql, val)
-
-mydb.commit()
-
-print(mycursor.rowcount, "was inserted.")
-"""
-#print(results.prettify())
+    result += "{}, {}, {}, Link: {} \n\n".format(title,price,details,link)
 
 ### Send the result to email
-
 
 import smtplib
 import config
@@ -72,10 +32,9 @@ from email.mime.text import MIMEText
 
 def get_contacts(filename):
 
-    """
-    Return two lists names, emails containing names and email addresses
-    read from a file specified by filename.
-    """
+
+    #Return two lists names, emails containing names and email addresses
+    #read from a file specified by filename.
     
     names = []
     emails = []
@@ -86,10 +45,10 @@ def get_contacts(filename):
     return names, emails
 
 def read_template(filename):
-    """
-    Returns a Template object comprising the contents of the 
-    file specified by filename.
-    """
+    
+    #Returns a Template object comprising the contents of the 
+    #file specified by filename.
+    
     
     with open(filename, 'r', encoding='utf-8') as template_file:
         template_file_content = template_file.read()
@@ -102,7 +61,7 @@ def main():
     # set up the SMTP server
     s = smtplib.SMTP(host=config.SERVER, port=config.PORT)
     s.starttls()
-    s.login(config.EMAIL config.PASSWORD)
+    s.login(config.EMAIL, config.PASSWORD)
 
     # For each contact, send the email:
     for name, email in zip(names, emails):
@@ -112,7 +71,7 @@ def main():
         message = message_template.substitute(PERSON_NAME=name.title())
 
         # Prints out the message body for our sake
-        print(message)
+        print(result)
 
         # setup the parameters of the message
         msg['From']=config.EMAIL
@@ -120,7 +79,7 @@ def main():
         msg['Subject']="Latest ads"
         
         # add in the message body
-        msg.attach(MIMEText(message, 'plain'))
+        msg.attach(MIMEText(result, 'plain'))
         
         # send the message via the server set up earlier.
         s.send_message(msg)
